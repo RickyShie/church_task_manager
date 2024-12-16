48437b59e8edfc7c210a16f5cd0f466dddf0c00a
christine <rickyshie0724@gmail.com>
2024-12-16 09:43:28 +0900

    Add a path displaying schedules by department.

--- schedule/urls.py
+++ schedule/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from .views import DepartmentScheduleView, AllSchedulesView
-from .views import assign_role
 
 urlpatterns = [
     path('schedules/', AllSchedulesView.as_view(), name='all_schedules'),
-    path('schedules/<str:department_name>/', DepartmentScheduleView.as_view(), name='department_schedules'),
-    path('assign-role/', assign_role, name='assign_role'),
+    path('schedules/<str:department_name>/', DepartmentScheduleView.as_view(), name='department_schedules')
 ]
