942a91bdbe9c66f144d2902e0e0c67fc03d763f1
christine <rickyshie0724@gmail.com>
2024-12-17 09:56:18 +0900

    Add a url pattern for hymn classes.

--- schedule/urls.py
+++ schedule/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import DepartmentScheduleView, AllSchedulesView
+from .views import DepartmentScheduleView, AllSchedulesView, HymnClassesView
 
 urlpatterns = [
     path('schedules/', AllSchedulesView.as_view(), name='all_schedules'),
-    path('schedules/<str:department_name>/', DepartmentScheduleView.as_view(), name='department_schedules')
+    path('schedules/<str:department_name>/', DepartmentScheduleView.as_view(), name='department_schedules'),
+    path('schedules/hymn_classes', HymnClassesView.as_view(), name="hymn_class_schedules")
 ]
